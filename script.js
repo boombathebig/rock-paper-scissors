1a137e40272a7dcf96ed4f648c9b762cac17ab1d
John Miller <jmiller55890@gmail.com>
2022-01-12 09:06:37 -0500

    added feature that running score will display on score board.  Also changed round() to push text content to scoreboard instead of console.log.

--- script.js
+++ script.js
@@ -2,69 +2,77 @@
    // Define a variable for the computerScore and the playerScore // 
    let playerScore= 0  
    let computerScore= 0;
+   // event listeners tying buttons (pictures) to player choices
+
    // Creates a function that randomly selects rock, paper or scissors for computerChoice
    function computerPlay() {
     let rnd= Math.random();
 
     if (rnd <= 0.33){
-           return `Rock`;
+           return `rock`;
        } else if (rnd <= 0.66){
-           return `Paper`;
+           return `paper`;
        } else {
-           return `Scissors`;
+           return `scissors`;
        }
    }
    // ROUND FUNCTION, PROMPTS USER FOR INPUT, COMPARES TO RANDOMLY GENERATED RESPONSE FROM COMPUTER, ADDS TO OVERALL
    // SCORE VARIABLES BELOW, PRINTS OUT THE CURRENT SCORE.
-   function round() {
+   function round(pChoice) {
 
-    playerSelection = prompt(`Please choose Rock, Paper, or Scissors`);
+    playerSelection = pChoice;
+    console.log(playerSelection)
     computerSelection = computerPlay();
+    
        if (playerSelection === `rock`){
-           if (computerSelection === `Rock`){
-            console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.  It's a tie!`);
-           } else if (computerSelection === `Paper`){
-            console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose!`);
+           if (computerSelection === `rock`){
+            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}. It's a Tie!`);
+            
+           } else if (computerSelection === `paper`){
+            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose!`);
             computerScore++;
            } else{
-            console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
+            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
             playerScore++;
            }
 
        }else if(playerSelection ==="paper") {
-        if (computerSelection === `Rock`){
-            console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
+        if (computerSelection === `rock`){
+            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
             playerScore++;
-           } else if (computerSelection === `Paper`){
-            console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.  It's a Tie!`);
+           } else if (computerSelection === `paper`){
+            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  It's a Tie!`);
            } else{
-            console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose!`);
+            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose!`);
             computerScore++;
            }
         }else if(playerSelection=== `scissors`){
-        if (computerSelection === `Rock`){
-            console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose`);
+        if (computerSelection === `rock`){
+            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose`);
             computerScore++;
-           } else if (computerSelection === `Paper`){
-            console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
+           } else if (computerSelection === `paper`){
+            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
             playerScore++
            } else{
-            console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.  It's a Tie!`);
+            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  It's a Tie!`);
            }
        } else{
            throw `You didn't put in Rock Paper or Scissors... try again...`;
        }
-       scoreAnnounce();
+       runningScore.textContent=(`The Current Score is Player: ${playerScore} to Computer: ${computerScore}`);
    }
-// event listeners tying buttons (pictures) to player choices
-const buttons= document.querySelectorAll(".images");
-// identify all three buttons
 
-buttons[1].addEventListener("click", function() {
-    round();
-    console.log("");
-})
-// add event listeners for each one, on click assign the id of the image to the computer choice variable 
+// identify all three buttons
+const buttons = document.querySelectorAll(`.images`)
+buttons.forEach((btn) => {btn.addEventListener("click",(e) => {
+    round(e.target.id);
+})});
+
+// identify the scoreboard
+scoreBoard = document.querySelector('.score');
+runningScore= document.querySelector(`.runningScore`)
+
+
 // the event listener should also run the game. 
 
 // loop, runs through round function 5x and reports out winner at the end. 
