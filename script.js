8ee398cd92eb103c934dd49078bb90d62585bfa8
John Miller <jmiller55890@gmail.com>
2022-01-13 16:48:02 -0500

    Created a score-check function to run each round.  Announces an overall winner after one of the participants reaches 5 points.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
    // Define a variable for the computerScore and the playerScore // 
    let playerScore= 0  
    let computerScore= 0;
-   // event listeners tying buttons (pictures) to player choices
+   
 
    // Creates a function that randomly selects rock, paper or scissors for computerChoice
    function computerPlay() {
@@ -16,7 +16,7 @@
            return `scissors`;
        }
    }
-   // ROUND FUNCTION, PROMPTS USER FOR INPUT, COMPARES TO RANDOMLY GENERATED RESPONSE FROM COMPUTER, ADDS TO OVERALL
+   // ROUND FUNCTION, on user button click, COMPARES TO RANDOMLY GENERATED RESPONSE FROM COMPUTER, ADDS TO OVERALL
    // SCORE VARIABLES BELOW, PRINTS OUT THE CURRENT SCORE.
    function round(pChoice) {
 
@@ -24,43 +24,44 @@
     console.log(playerSelection)
     computerSelection = computerPlay();
     
-       if (playerSelection === `rock`){
-           if (computerSelection === `rock`){
-            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}. It's a Tie!`);
-            
-           } else if (computerSelection === `paper`){
-            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose!`);
-            computerScore++;
-           } else{
-            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
-            playerScore++;
-           }
+            if (playerSelection === `rock`){
+                if (computerSelection === `rock`){
+                    scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}. It's a Tie!`);
+                    
+                } else if (computerSelection === `paper`){
+                    scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose!`);
+                    computerScore++;
+                } else{
+                    scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
+                    playerScore++;
+                }
 
-       }else if(playerSelection ==="paper") {
-        if (computerSelection === `rock`){
-            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
-            playerScore++;
-           } else if (computerSelection === `paper`){
-            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  It's a Tie!`);
-           } else{
-            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose!`);
-            computerScore++;
-           }
-        }else if(playerSelection=== `scissors`){
-        if (computerSelection === `rock`){
-            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose`);
-            computerScore++;
-           } else if (computerSelection === `paper`){
-            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
-            playerScore++
-           } else{
-            scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  It's a Tie!`);
-           }
-       } else{
-           throw `You didn't put in Rock Paper or Scissors... try again...`;
-       }
-       runningScore.textContent=(`The Current Score is Player: ${playerScore} to Computer: ${computerScore}`);
-   }
+            }else if(playerSelection ==="paper") {
+                if (computerSelection === `rock`){
+                    scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
+                    playerScore++;
+                } else if (computerSelection === `paper`){
+                    scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  It's a Tie!`);
+                } else{
+                    scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose!`);
+                    computerScore++;
+                }
+                }else if(playerSelection=== `scissors`){
+                if (computerSelection === `rock`){
+                    scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Lose`);
+                    computerScore++;
+                } else if (computerSelection === `paper`){
+                    scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  You Win!`);
+                    playerScore++
+                } else{
+                    scoreBoard.textContent=(`You chose ${playerSelection} and the computer chose ${computerSelection}.  It's a Tie!`);
+                }
+            } else{
+                throw `You didn't put in Rock Paper or Scissors... try again...`;
+            }
+            runningScore.textContent=(`The Current Score is Player: ${playerScore} to Computer: ${computerScore}`);
+            checkScore();
+        }
 
 // identify all three buttons
 const buttons = document.querySelectorAll(`.images`)
@@ -72,8 +73,15 @@ buttons.forEach((btn) => {btn.addEventListener("click",(e) => {
 scoreBoard = document.querySelector('.score');
 runningScore= document.querySelector(`.runningScore`)
 
-
-// the event listener should also run the game. 
-
-// loop, runs through round function 5x and reports out winner at the end. 
-// function for announcing total score
+// function to check score & clear score if either side hits 5. 
+function checkScore(){ 
+    if (playerScore ===5) {
+    runningScore.textContent=(`YOU WIN, FIRST TO FIVE! PLAYER: ${playerScore}  COMPUTER: ${computerScore}. Choose Rock, Paper, or Scissors to play again`);
+    computerScore=0;
+    playerScore=0;
+    } else if(computerScore ===5){
+        runningScore.textContent=(`YOU LOSE, FIRST TO FIVE! PLAYER: ${playerScore}  COMPUTER: ${computerScore}. Choose Rock, Paper, or Scissors to play again`);
+        computerScore=0;
+        playerScore=0;
+    }
+};
